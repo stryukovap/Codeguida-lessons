d7ee58b2fecf868d47cb933c3bfdcd3b31a06e8a
Andrey <31798016+stryukovap@users.noreply.github.com>
2017-11-19 10:44:44 +0200

    lesson 3 all

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 #import models
@@ -16,25 +17,20 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first()  # TODO: отримати в даний час залогіненого користувача
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)  # TODO: редірект на створену сторінку теми
-
-    return render(request, 'new_topic.html', {'board': board})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: перенаправити до створеної сторінки теми
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
